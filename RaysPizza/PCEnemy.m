065c17020ff953fbe203c615d8ef09a72e998f57
Pasquale Barilla <pat.barilla@gmail.com>
2014-06-19 09:36:30 +1000

    fleshed out enemy class more.

--- RaysPizza/PCEnemy.m
+++ RaysPizza/PCEnemy.m
@@ -10,4 +10,45 @@
 
 @implementation PCEnemy
 
+-(id)initWithClass:(int)EnemyClass {
+    self = [super init];
+    if (self) {
+        currentClass = EnemyClass;
+    }
+    return self;
+}
+
+-(void)setState:(int)EnemyState {
+    currentState = EnemyState;
+}
+
+-(int)checkHealth {
+    return health;
+}
+
+-(void)setStartingLocation:(CGPoint)startingLocation {
+    currentPosition = startingLocation;
+}
+
+-(BOOL)checkHearing {
+    // algorithm to check hearing
+    return NO;
+}
+
+-(BOOL)checkVisual {
+    // algorithm to check visual
+    return NO;
+}
+
+-(CGPoint)findTarget {
+    // check visual contact and return position
+    CGPoint target = CGPointMake(0,0);
+    return target;
+}
+
+-(void)setSpawnTimer:(float)timeToSpawn {
+    
+}
+
+
 @end
